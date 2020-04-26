8835c7e03417716140071c68d23766395597913e
Alexander Egeberg <Alexanderegeberg@hotmail.com>
2020-04-26 21:52:52 +0200

    Lagt op

---
+++ DBObligatoriskOpgave/DBObligatoriskOpgave/ManageFacility.cs
@@ -0,0 +1,127 @@
+﻿using DBObligatoriskOpgaveWS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBObligatoriskOpgave
+{
+    class ManageFacility
+    {
+        const string serverUrl = "http://localhost:62376";
+
+
+        public static async void ReadFacilities()
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.UseDefaultCredentials = true;
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(serverUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                List<Hotel> facilitiesList = new List<Hotel>();
+
+                try
+                {
+                    var response = client.GetAsync("api/Facilities").Result;
+                    var facilities = response.Content.ReadAsAsync<IEnumerable<Facility>>().Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        foreach (var item in facilities)
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }
+
+                }
+
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        public static async void CreateFacility(Facility facilityObj)
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.UseDefaultCredentials = true;
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(serverUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    var response = client.PostAsJsonAsync("api/Facilities/", facilityObj).Result;
+                    Console.WriteLine("Facility created");
+                }
+
+                catch (Exception)
+                {
+                    throw;
+                }
+
+            }
+
+        }
+
+        public static async void DeleteFacility(int id)
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.UseDefaultCredentials = true;
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(serverUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    var response = client.DeleteAsync("api/Facilities/" + id).Result;
+                    Console.WriteLine("Your facility has now been deleted");
+                }
+
+                catch (Exception)
+                {
+                    throw;
+                }
+
+            }
+
+        }
+
+        public static async void UpdateFacility(int id, Facility facilityObj)
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.UseDefaultCredentials = true;
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(serverUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    var response = client.PutAsJsonAsync("api/Facilities/" + id, facilityObj).Result;
+                    Console.WriteLine("Your facility is now updated");
+                }
+
+                catch (Exception)
+                {
+                    throw;
+                }
+
+            }
+
+        }
+
+    }
+}
---
+++ DBObligatoriskOpgave/DBObligatoriskOpgave/Program.cs
@@ -0,0 +1,37 @@
+﻿using DBObligatoriskOpgaveWS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBObligatoriskOpgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /// Create
+            ManageFacility.CreateFacility(new Facility() {Facility_No = 7, Facility_Name = "Biograf" });
+
+            /// Read
+            ManageFacility.ReadFacilities();
+
+            /// Update
+            ManageFacility.UpdateFacility(7, new Facility() { Facility_No = 7, Facility_Name = "3D Biograf" });
+
+
+            ManageFacility.ReadFacilities();
+
+            /// Delete
+
+            ManageFacility.DeleteFacility(7);
+
+            ManageFacility.ReadFacilities();
+
+            Console.ReadKey();
+
+        }
+    }
+}
